8c09d2ea2c884d80736288af941968adf9a0966d
Sonia <soniawadji@gmail.com>
2022-08-19 19:56:35 +0530

    Created a data class for github service

--- app/src/main/java/com/hangrycoder/githubrequests/GithubService.kt
+++ app/src/main/java/com/hangrycoder/githubrequests/GithubService.kt
@@ -1,9 +1,11 @@
 package com.hangrycoder.githubrequests
 
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface GithubService {
+
     @GET("repos/square/retrofit/pulls")
-    fun getPullRequests(@Query("state") state: String)
+    fun getPullRequests(@Query("state") state: String): Call<List<PullRequest>>
 }
---
+++ app/src/main/java/com/hangrycoder/githubrequests/PullRequest.kt
@@ -0,0 +1,3 @@
+package com.hangrycoder.githubrequests
+
+data class PullRequest(val title: String)
