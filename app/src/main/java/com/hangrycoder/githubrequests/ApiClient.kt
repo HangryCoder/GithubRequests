8f8d06da9982923d2aff63069b2f0544309f4a97
Sonia <soniawadji@gmail.com>
2022-08-20 14:21:43 +0530

    Added HTTP logging interceptor. Api is getting called. But failing while parsing

--- app/src/main/java/com/hangrycoder/githubrequests/ApiClient.kt
+++ app/src/main/java/com/hangrycoder/githubrequests/ApiClient.kt
@@ -1,6 +1,7 @@
 package com.hangrycoder.githubrequests
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,7 +9,9 @@ object ApiClient {
     val BASE_URL = "https://api.github.com/"
 
     fun getClient(): Retrofit {
-        val client = OkHttpClient.Builder().build()
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
 
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
