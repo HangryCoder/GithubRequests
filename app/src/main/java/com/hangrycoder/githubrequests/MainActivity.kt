18c91b8f664e486ddc79d13af001490c520cac5d
Sonia <soniawadji@gmail.com>
2022-08-20 12:43:26 +0530

    Added view binding

--- app/src/main/java/com/hangrycoder/githubrequests/MainActivity.kt
+++ app/src/main/java/com/hangrycoder/githubrequests/MainActivity.kt
@@ -2,11 +2,15 @@ package com.hangrycoder.githubrequests
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.hangrycoder.githubrequests.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
