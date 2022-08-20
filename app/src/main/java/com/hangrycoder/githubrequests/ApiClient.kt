337b2131b218e59b60a4c40c294ae16d831320d6
Sonia <soniawadji@gmail.com>
2022-08-20 17:13:36 +0530

    Cleaned up some code

--- app/src/main/java/com/hangrycoder/githubrequests/ApiClient.kt
+++ app/src/main/java/com/hangrycoder/githubrequests/ApiClient.kt
@@ -1,7 +1,6 @@
 package com.hangrycoder.githubrequests
 
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -15,36 +14,14 @@ object ApiClient {
         val httpLoggingInterceptor = HttpLoggingInterceptor()
         httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
         val client = OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
 
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addCallAdapterFactory(NetworkResponseAdapterFactory())
-            .addConverterFactory(
-                //Might need to delete this
-                MoshiConverterFactory.create(
-                    Moshi.Builder()
-                        /*.add(
-                            PolymorphicJsonAdapterFactory.of(
-                                NetworkResponse::class.java,
-                                "response"
-                            ).withSubtype(NetworkResponse.Success::class.java, "success")
-                                .withSubtype(
-                                    NetworkResponse.ServerError::class.java,
-                                    "server_error"
-                                )
-                                .withSubtype(
-                                    NetworkResponse.NetworkError::class.java,
-                                    "network_error"
-                                )
-                                .withSubtype(
-                                    NetworkResponse.UnknownError::class.java,
-                                    "unknown_error"
-                                )
-                        )*/
-                        .add(KotlinJsonAdapterFactory())
-                        .build()
-                )
-            )
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .client(client)
             .build()
     }
