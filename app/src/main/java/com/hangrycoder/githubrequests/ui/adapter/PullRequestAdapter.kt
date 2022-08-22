7d526dc073d1ea6312940f3561a37b07bbebc642
Sonia <soniawadji@gmail.com>
2022-08-22 08:35:57 +0530

    Using view binding in PullRequestAdapter.kt

--- app/src/main/java/com/hangrycoder/githubrequests/ui/adapter/PullRequestAdapter.kt
+++ app/src/main/java/com/hangrycoder/githubrequests/ui/adapter/PullRequestAdapter.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.hangrycoder.githubrequests.R
+import com.hangrycoder.githubrequests.databinding.PullRequestItemBinding
 import com.hangrycoder.githubrequests.models.PullRequest
 import com.hangrycoder.githubrequests.utils.DateUtil
 
@@ -18,13 +19,24 @@ class PullRequestAdapter(diffCallback: DiffUtil.ItemCallback<PullRequest>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PullRequestViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.layout_pull_request_item, parent, false)
+            .inflate(R.layout.pull_request_item, parent, false)
         return PullRequestViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: PullRequestViewHolder, position: Int) {
-        val pullRequest = getItem(position)//pullRequests?.get(position)
-        with(holder) {
+        val pullRequest = getItem(position)
+        holder.bind(pullRequest)
+    }
+
+    inner class PullRequestViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = PullRequestItemBinding.bind(itemView)
+        private val authorNameTextView: AppCompatTextView = binding.pullRequestUserName
+        private val authorImageView: AppCompatImageView = binding.pullRequestUserImage
+        private val titleTextView: AppCompatTextView = binding.pullRequestTitle
+        private val createdAtTextView: AppCompatTextView = binding.createdDate
+        private val closedAtTextView: AppCompatTextView = binding.closedDate
+
+        fun bind(pullRequest: PullRequest?) {
             authorNameTextView.text = pullRequest?.user?.name
             titleTextView.text = pullRequest?.title
             createdAtTextView.text = DateUtil.convertTimestampToDate(pullRequest?.createdAt)
@@ -35,24 +47,6 @@ class PullRequestAdapter(diffCallback: DiffUtil.ItemCallback<PullRequest>) :
                 .circleCrop()
                 .placeholder(R.drawable.avatar_placeholder)
                 .into(authorImageView)
-        }
-    }
-
-    inner class PullRequestViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val authorNameTextView: AppCompatTextView
-        val authorImageView: AppCompatImageView
-        val titleTextView: AppCompatTextView
-        val createdAtTextView: AppCompatTextView
-        val closedAtTextView: AppCompatTextView
-
-        init {
-            with(itemView) {
-                authorNameTextView = findViewById(R.id.pull_request_user_name)
-                authorImageView = findViewById(R.id.pull_request_user_image)
-                titleTextView = findViewById(R.id.pull_request_title)
-                createdAtTextView = findViewById(R.id.created_date)
-                closedAtTextView = findViewById(R.id.closed_date)
-            }
         }
     }
 }
