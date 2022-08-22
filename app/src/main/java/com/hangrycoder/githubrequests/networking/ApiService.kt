d904c90cc707fd70e2369bf09260080c7c0531b4
Sonia <soniawadji@gmail.com>
2022-08-22 08:26:51 +0530

    Switched to fetching glide pull requests

--- app/src/main/java/com/hangrycoder/githubrequests/networking/ApiService.kt
+++ app/src/main/java/com/hangrycoder/githubrequests/networking/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("repos/square/retrofit/pulls")
+    @GET("repos/bumptech/glide/pulls")
     suspend fun getPullRequests(
         @Query("state") state: String,
         @Query("page") page: Int
