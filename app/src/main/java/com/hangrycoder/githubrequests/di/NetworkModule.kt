2dba2221a38460aa1ed560cc16349f5f5ca93524
Sonia <soniawadji@gmail.com>
2022-08-31 10:26:27 +0530

    Created a NetworkModule.kt

---
+++ app/src/main/java/com/hangrycoder/githubrequests/di/NetworkModule.kt
@@ -0,0 +1,50 @@
+package com.hangrycoder.githubrequests.di
+
+import com.hangrycoder.githubrequests.networking.ApiService
+import com.hangrycoder.githubrequests.networking.NetworkResponseAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@Module
+class NetworkModule(val baseUrl: String) {
+
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().also { it.level = HttpLoggingInterceptor.Level.BODY }
+    }
+
+    @Provides
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addCallAdapterFactory(NetworkResponseAdapterFactory())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+}
