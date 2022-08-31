39b432ef414b0f5222b2acc6403fe7409124ee4f
Sonia <soniawadji@gmail.com>
2022-08-31 17:01:49 +0530

    Created a new activity scope

--- app/src/main/java/com/hangrycoder/githubrequests/di/ActivityComponent.kt
+++ app/src/main/java/com/hangrycoder/githubrequests/di/ActivityComponent.kt
@@ -3,6 +3,7 @@ package com.hangrycoder.githubrequests.di
 import com.hangrycoder.githubrequests.ui.MainActivity
 import dagger.Subcomponent
 
+@ActivityScope
 @Subcomponent(modules = [ActivityModule::class])
 interface ActivityComponent {
     @Subcomponent.Factory
